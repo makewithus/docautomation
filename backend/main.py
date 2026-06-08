7756bed3c84a2d50691e99a074666367e20c4c32
Nimisha <nimishaagrawal115@gmail.com>
2026-06-08 15:34:39 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -48,17 +48,24 @@ app = FastAPI(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 # ── CORS ──────────────────────────────────────────────────────────────────
 # ── CORS ──────────────────────────────────────────────────────────────────
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=[
+#         "http://localhost:3000",
+#         "http://localhost:5173",
+#         "http://127.0.0.1:3000",
+#         "http://127.0.0.1:5173",
+
+#         # Vercel frontend
+#         "https://docautomation-tawny.vercel.app",
+#     ],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173",
-
-        # Vercel frontend
-        "https://docautomation-tawny.vercel.app",
-    ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
